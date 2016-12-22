c115c6e6867422df8dd8c8f3e74ed7eddf7b5bd6
Harlos <andrey.bykov5230@gmail.com>
2016-12-22 21:18:49 +0200

    first commit

---
+++ ball.js
@@ -0,0 +1,55 @@
+function Ball() {
+  this.x = 250;
+  this.y = 250;
+  this.dx=0;
+  this.dy = 0;
+  this.radius = 30;
+  
+  this.update = function() {
+    if ( this.y + this.dy + this.radius > height || this.y + this.dy - this.radius < 0 ) {
+    	this.dy > 0 ? this.dy -= 0.1 : this.dy += 0.1;
+    	this.dy *= -1;
+    }//if it goes off the screen
+    
+    if ( this.x + this.dx + this.radius > width || this.x + this.dx - this.radius < 0 ) {
+    	this.dx *= -1;
+    	this.dx > 0 ? this.dx -= 0.1 : this.dx += 0.1; 
+    }//if it goes off the screen
+    
+  	this.dy += 1; //gravity
+  	this.dy > 0 ? this.dy *= 0.98 : this.dy *= 0.98; //air resistance
+  	this.dx > 0 ? this.dx *= 0.98 : this.dx *= 0.98; //air resistance
+  	if(this.y + this.radius > height && this.dy <= 0.2) this.dy = 0;  //for not slightly jumping
+  	if(this.dx >= -0.2 && this.dx <= 0.2) this.dx=0; //for not moving slow
+  	this.x += this.dx;
+  	this.y += this.dy;
+  	
+  	if(this.x + this.radius > width) this.x = width - this.radius;
+  	if(this.x - this.radius < 0) this.x = this.radius;
+  	if(this.y + this.radius > height) this.y = height - this.radius;
+  	if(this.y - this.radius < 0) this.y = this.radius;
+  };
+  
+  this.move = function(x,y) {
+  	if(x + this.radius > width) x = width - this.radius;
+  	if(x - this.radius < 0) x = this.radius;
+  	if(y + this.radius > height) y = height - this.radius;
+  	if(y - this.radius < 0) y = this.radius;
+  	this.x = x;
+  	this.y = y;
+  };
+  
+  this.changeRad = function(rad){
+  	this.radius=rad;
+  }
+  
+  this.setdifference = function(dx,dy){
+  	this.dx = 0.2 * dx;
+  	this.dy = 0.2 * dy;
+  }
+  
+  this.display = function() {
+  	fill(200,0,0);
+  	ellipse(this.x, this.y, this.radius * 2, this.radius * 2)
+  };
+}
---
+++ sketch.js
@@ -0,0 +1,48 @@
+var mem = [];
+var dx, dy;
+var onBall = false;
+p5.disableFriendlyErrors = true;
+function setup() {
+  createCanvas(windowWidth, windowHeight-50);
+  ball = new Ball();
+  slider = createSlider(10,50,25);
+  slider.input(loga);
+}
+
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight-50);
+}
+
+function loga(){
+	ball.changeRad(slider.value());
+}
+
+function mousePressed(){
+	if(dist(ball.x,ball.y,mouseX,mouseY)<=ball.radius)
+	{
+		onBall=true;
+		dx = mouseX - ball.x; 
+		dy = mouseY - ball.y;
+	}
+}
+
+function pressed(){
+	ball.move(mouseX - dx, mouseY - dy);
+  if(mem.length > 5) mem.splice(0,1);
+  mem.push([mouseX - dx, mouseY - dy]);
+}
+
+function mouseReleased(){
+	if(mem.length)	ball.setdifference(mem[mem.length-1][0] - mem[0][0], mem[mem.length-1][1] - mem[0][1]);
+  onBall = false;
+  mem = [];
+}
+
+function draw() {
+  background(100);
+  if(onBall) 
+  	pressed();
+  else
+  	ball.update();
+  ball.display();
+}
